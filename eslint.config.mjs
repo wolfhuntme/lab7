1ecb4e35857f5229cf5bd671c56880efcfb1056c
wolfhuntme <50736074+wolfhuntme@users.noreply.github.com>
2025-07-22 17:35:17 +0800

    123

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,22 @@
-import js from "@eslint/js";
 import globals from "globals";
+import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-]);
+  {
+    plugins: {
+      security: pluginSecurity
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...pluginReact.configs.flat.recommended.rules,
+      "security/detect-eval-with-expression": "error",
+    }
+  }
+];
